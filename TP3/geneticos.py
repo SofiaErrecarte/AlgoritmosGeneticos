7a13d34c368f2b3a9be8561d2fd290e475bfeff3
sofiaerrecarte <sofiaerrecarte@gmail.com>
2021-09-13 19:37:40 -0300

    geneticos v3

--- TP3/geneticos.py
+++ TP3/geneticos.py
@@ -7,8 +7,8 @@ import pandas as pd
 cant_poblacion = 50
 listaNumeros = np.arange(0,24)
 matriz_distancias = []
-prob_mutacion= 0.05
-prob_crossover=0.9
+prob_mutacion= 0.03
+prob_crossover=0.75
 ciclos=200
 minimos=[]
 maximos=[]
@@ -56,8 +56,8 @@ def cargaMatrizDeDistancias():
 def crear_poblacion():    
     poblacion = []
     for i in range(cant_poblacion):
-        poblacion.append(rand.sample(list(listaNumeros), 24))
-        poblacion[i].append(poblacion[i][0])        
+        poblacion.append(rand.sample(list(listaNumeros), 24)) #crea una lista random de 0 a 24
+        poblacion[i].append(poblacion[i][0]) #agrega la capital inicial al final        
     return poblacion
 
 def calcular_funcion_objetivo(poblacion):
@@ -69,7 +69,6 @@ def calcular_funcion_objetivo(poblacion):
         suma+=matriz_distancias[poblacion[i][j+1]][poblacion[i][0]]
         funcion_obj.append(suma)
     print(min(funcion_obj))
-   
     return funcion_obj
 
 def calcular_fitness(funcion_obj):
@@ -122,83 +121,23 @@ def crossoverCiclico(padre1, padre2):
     
     hijo1.append(hijo1[0])
     hijo2.append(hijo2[0])
-    #print('hijo1 ', hijo1)
-    #print('hijo2', hijo2)
     return hijo1,hijo2
 
-def elitismo(nuevaPoblacion,list_fitness):
-    ind_ordenes = []
-    #aux = lista_fit.copy()
-
-    ind_ordenes = np.argsort(list_fitness) #Ordena los indices de menor a mayor
-    list(ind_ordenes)
-    #print(len(ind_ordenes))
-    #print(ind_ordenes)
-    for i in range(10):
-        #print(len(ind_ordenes)-(i+1))
-        nuevaPoblacion.append(poblacion[ind_ordenes[len(ind_ordenes)-(i+1)]])  #Agrega el miembro con el indice del mejor fitness, de mayor a menor.
-       
-        
-    #print(len(nuevaPoblacion))
-    return nuevaPoblacion
-
-def crossover(poblacion,list_fitness,funcion_obj):
+def crossover(poblacion,list_fitness):
     ruleta = crear_ruleta(list_fitness)
     x = rand.uniform(0,1)
     nuevaPoblacion = []
     i = 0
-    for j in range(25): #20 chances de crossover entre 40 miembros.
+    for _ in range(25): 
         if(x<=prob_crossover):
-            nuevoCromo = []
-            for k in range(24):
-                nuevoCromo.append(31) #31 es un valor default para saber que fue cambiado y que no.
             ind1 = ruleta[i]
             ind2 = ruleta[i+1]        
             reco1 = poblacion[ind1]
-            reco2 = poblacion[ind2]    # Selecciono dos recorridos con la ruleta.
-            for k in range(len(reco1)):
-                if (k==0):
-                    nuevoCromo[0] = reco1[0]
-                    vola = reco2[0]
-                else:
-                   dumpind =  reco1.index(vola)    
-                   nuevoCromo[dumpind] = reco1[dumpind]
-                   vola = reco2[dumpind]
-                   if(reco2[dumpind] == reco1[0]):               
-                       break            
-            for k in range(len(nuevoCromo)):
-                if(nuevoCromo[k]==31):
-                    nuevoCromo[k] = reco2[k]
-            nuevaPoblacion.append(nuevoCromo) #Agrego nuevo recorrido a la poblacion
-            
-            #Realizo mismo proceso forma simétrica.
-
-            nuevoCromo = []
-            for k in range(24):
-                nuevoCromo.append(31)
-            ind1 = ruleta[i+1]
-            ind2 = ruleta[i]   
-            reco1 = poblacion[ind1]
-            reco2 = poblacion[ind2]   
-            for k in range(len(reco1)):
-                if (k==0):
-                    nuevoCromo[0] = reco1[0]
-                    vola = reco2[0]
-                else:
-                    dumpind =  reco1.index(vola) 
-                    nuevoCromo[dumpind] = reco1[dumpind]
-                    vola = reco2[dumpind]
-                    if(reco2[dumpind] == reco1[0]):             
-                        break 
-                  
-            for k in range(len(nuevoCromo)):
-                if(nuevoCromo[k]==31):
-                    nuevoCromo[k] = reco2[k]
-            nuevaPoblacion.append(nuevoCromo)
-            i+=2 #Aumento en 2 el bucle for
-
-
-
+            reco2 = poblacion[ind2] 
+            padre1,padre2=crossoverCiclico(reco1,reco2)
+            nuevaPoblacion.append(padre1)
+            nuevaPoblacion.append(padre2)
+            i+=2
         elif(x>prob_crossover):
             nuevaPoblacion.append(poblacion[i])
             nuevaPoblacion.append(poblacion[i+1])
@@ -206,24 +145,47 @@ def crossover(poblacion,list_fitness,funcion_obj):
     #print(len(nuevaPoblacion))
     return nuevaPoblacion
 
-def mutacion(nuevaPoblacion):
-    """ Chequea mutación para los 40 miembros post-crossover"""
-    for i in range(40):
-        x = rand.uniform(0,1)
-        #print(nuevaPoblacion[i])
-        if(x<=prob_mutacion):
-            #print("muto jojo")
-            ale1 = rand.randrange(0,23)
-            ale2 = rand.randrange(0,23)
-            #print(ale1)
-            #print(ale2)
-            while(ale2 == ale1):
-                ale2 = rand.randrange(0,23)
-            aux = nuevaPoblacion[i][ale1]
-            nuevaPoblacion[i][ale1] = nuevaPoblacion[i][ale2]
-            nuevaPoblacion[i][ale2] = aux
-            #print(nuevaPoblacion[i])
+def elite(poblacion,list_fitness,funcion_obj):
+    ruleta = crear_ruleta(list_fitness)
+    nuevaPoblacion = []
+    indices = np.array(funcion_obj)
+    indices=indices.argsort() 
+    for i in range(10):
+        j=indices[i]
+        nuevaPoblacion.append(poblacion[j])
+    for i in range(10):
+        print(nuevaPoblacion[i])
+    x = rand.uniform(0,1)
+    i = 0
+    for _ in range(20): 
+        if(x<=prob_crossover):
+            ind1 = ruleta[i]
+            ind2 = ruleta[i+1]        
+            reco1 = poblacion[ind1]
+            reco2 = poblacion[ind2] 
+            padre1,padre2=crossoverCiclico(reco1,reco2)
+            nuevaPoblacion.append(padre1)
+            nuevaPoblacion.append(padre2)
+            i+=2
+        elif(x>prob_crossover):
+            nuevaPoblacion.append(poblacion[i])
+            nuevaPoblacion.append(poblacion[i+1])
+            i+=2
     return nuevaPoblacion
+
+def mutacion(poblacion):
+    x=rand.random()
+    for i in range(50):
+        if(x <= prob_mutacion):
+            r1 = rand.randrange(0,23)
+            r2 = rand.randrange(0,23)
+            while(r1==r2):
+                r1=rand.randrange(0,23)
+            aux = poblacion[i][r1]
+            poblacion[i][r1] = poblacion[i][r2]
+            poblacion[i][r2] = aux
+    return poblacion
+
 def tabla():
     generacion = np.arange(1, ciclos + 1)
     
@@ -241,27 +203,54 @@ def tabla():
     worksheet.conditional_format("D1:DF"+str(len(promedios)+1), {"type": "3_color_scale", "max_color": "red", "mid_color": "yellow", "min_color": "green"})
 
     Tabla.save()
+
+def crossover_rango(list_fitness,poblacion):
+    nuevaPoblacion = []
+    rango=[]
+    print(list_fitness)
+    indices = np.array(list_fitness)
+    indices=indices.argsort() 
+    print(indices)
+    for i in range(20,50):
+        j=indices[i]
+        rango.append(poblacion[j])
+    '''for i in range(30):
+        print(nuevaPoblacion[i])'''
+    for i in range(30):
+        nuevaPoblacion.append(rango[i])
+    print('poblacion ',nuevaPoblacion)
+    for i in range (0,20,2):#20 son los que reemplazo por crossover entre los 30 elegidos 
+        padre_1 = rand.choice(rango)
+        padre_2 = rand.choice(rango)
+        if (rand.random() <= prob_crossover):
+            padre_1,padre_2=crossoverCiclico(padre_1,padre_2)
+        nuevaPoblacion.append(padre_1)
+        nuevaPoblacion.append(padre_2)
+    for i in range(30):
+        nuevaPoblacion.append(rango[i])
+    print('nueva ',nuevaPoblacion)
+    return nuevaPoblacion
+
 cargaMatrizDeDistancias()
 poblacion= crear_poblacion()
-print('Poblacion inicial: ',poblacion)
+#print('Poblacion inicial: ',poblacion)
 for i in range(ciclos):
     funcion_obj = calcular_funcion_objetivo(poblacion)
     list_fitness = calcular_fitness(funcion_obj)
-    minimoObj = min(funcion_obj)
-    minimos.append(minimoObj)
+    dist_min = min(funcion_obj)
+    minimos.append(dist_min)
     maximos.append(max(funcion_obj))
     promedios.append(np.mean(funcion_obj))
-    indice = funcion_obj.index(minimoObj)
+    indice = funcion_obj.index(dist_min)
     optimo = poblacion[indice]
     optimos.append(optimo)
-    if (minimoObj < valor_cromosoma_optimo) or (valor_cromosoma_optimo == 0):
-        valor_cromosoma_optimo = minimoObj
+    if (dist_min < valor_cromosoma_optimo) or (valor_cromosoma_optimo == 0):
+        valor_cromosoma_optimo = dist_min
         cromosoma_optimo = optimo
-    nuevaPoblacion=crossover(poblacion,list_fitness,funcion_obj)
-    nuevaPoblacion=mutacion(nuevaPoblacion)
-    print('Poblacion: ',poblacion)
-    #poblacion=mutacion(poblacion)
-    poblacion=elitismo(nuevaPoblacion,list_fitness)
+    #poblacion=crossover(poblacion,list_fitness)
+    poblacion=crossover_rango(list_fitness,poblacion)
+    poblacion=mutacion(poblacion)
+
 tabla()
 print(promedios)
 print("Cromosoma óptimo: ", cromosoma_optimo)
